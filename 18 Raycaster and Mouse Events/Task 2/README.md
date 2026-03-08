d824dcc4fec691321db0a89413d2d05ab86fff45
fernoe1 <krutoytemirlan2007@mail.ru>
2026-03-08 14:23:41 +0600

    feat(raycstr && mousevnts): task two

---
+++ 18 Raycaster and Mouse Events/Task 2/README.md
@@ -0,0 +1,18 @@
+Continuation from the previous task.
+
+
+
+Remove all raycaster intersection part, only keep raycaster implementation.
+
+
+
+Animate all 3 spheres at different speeds using Math.sin().
+
+
+
+In each frame, cast a raycaster and whenever a sphere is hit, set its material.color to 0x0000ff.
+
+
+
+If the sphere is not being hit, set its color to 0xff0000 back.
+
---
+++ 18 Raycaster and Mouse Events/Task 2/solution.js
@@ -0,0 +1,128 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import GUI from 'lil-gui';
+const gui = new GUI({
+    title: 'debug',
+    width: 370,
+    closeFolders: true
+});
+const debugObject = {};
+
+addEventListener('keypress', (event) => {
+    if (event.key === 'h') gui.show(gui._hidden);
+});
+
+const canvas = document.querySelector('canvas.webgl');
+
+const sizes = {
+    width: innerWidth,
+    height: innerHeight
+};
+
+addEventListener('resize', () => {
+    sizes.width = innerWidth;
+    sizes.height = innerHeight;
+
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(2, devicePixelRatio));
+});
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.z = 3;
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(2, devicePixelRatio));
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+const object1 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+object1.position.x = -2;
+
+const object2 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+
+const object3 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+object3.position.x = 2;
+
+scene.add(object1, object2, object3);
+
+const raycaster = new THREE.Raycaster();
+
+// const rayOrigin = new THREE.Vector3(-3, 0, 0);
+// const rayDirection = new THREE.Vector3(10, 0, 0);
+// rayDirection.normalize();
+
+// raycaster.set(rayOrigin, rayDirection);
+
+// const intersect = raycaster.intersectObject(object1);
+// console.log(intersect);
+// const intersects = raycaster.intersectObjects([object1, object2, object3]);
+// console.log(intersects);
+
+const clock = new THREE.Clock();
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+
+    object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+    object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+    object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
+
+    const rayOrigin = new THREE.Vector3(-3, 0, 0);
+    const rayDirection = new THREE.Vector3(1, 0, 0);
+    rayDirection.normalize();
+    raycaster.set(rayOrigin, rayDirection);
+
+    const objectsToTest = [object1, object2, object3];
+    for (const object of objectsToTest) {
+        object.material.color.set(0xff0000);
+    }
+    const intersects = raycaster.intersectObjects(objectsToTest);
+    for (const intersect of intersects) {
+        intersect.object.material.color.set(0x0000ff);
+    }
+
+    controls.update();
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(tick);
+};
+
+tick();
+
+/** CSS
+*
+{
+    margin: 0;
+    padding: 0;
+}
+
+html,
+body
+{
+    overflow: hidden;
+}
+
+.webgl
+{
+    position: fixed;
+    top: 0;
+    left: 0;
+    outline: none;
+}
+*/
