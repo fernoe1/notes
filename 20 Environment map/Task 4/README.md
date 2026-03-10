3c3c0e009349e9acac5df05f6215d992932b999c
fernoe1 <krutoytemirlan2007@mail.ru>
2026-03-10 18:58:49 +0600

    feat(env map): task four

---
+++ 20 Environment map/Task 4/README.md
@@ -0,0 +1,14 @@
+Continuation from the previous task.
+
+
+
+Using NVIDIA AI Canvas, create a custom made environment map.
+
+
+
+Import EXRLoader, and instantiate it.
+
+
+
+Using EXRLoader, load the exr environment map.
+
---
+++ 20 Environment map/Task 4/solution.js
@@ -0,0 +1,158 @@
+import * as THREE from 'three';
+import { OrbitControls, GLTFLoader, RGBELoader, EXRLoader } from 'three/examples/jsm/Addons.js';
+import GUI from 'lil-gui';
+const gui = new GUI({
+    title: 'debug',
+    width: 370,
+    closeFolders: true
+});
+const global = {};
+
+addEventListener('keypress', (e) => {
+    if (e.key === 'h') gui.show(gui._hidden);
+});
+
+const canvas = document.querySelector('canvas.webgl');
+
+const gltfLoader = new GLTFLoader();
+const rgbeLoader = new RGBELoader();
+const exrLoader = new EXRLoader();
+
+const sizes = {
+    width: innerWidth,
+    height: innerHeight
+};
+
+addEventListener('resize', () => {
+    sizes.width = innerWidth;
+    sizes.height = innerHeight;
+
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(2, devicePixelRatio));
+});
+
+const scene = new THREE.Scene();
+
+const updateAllMaterials = () => {
+    scene.traverse((child) => {
+        if (child.isMesh && child.material.isMeshStandardMaterial) {
+            child.material.envMapIntensity = global.envMapIntensity;
+        }
+    })
+};
+
+scene.backgroundBlurriness = 0;
+gui
+    .add(scene, 'backgroundBlurriness')
+    .min(0)
+    .max(1)
+    .step(0.001);
+
+scene.backgroundIntensity = 1;
+gui
+    .add(scene, 'backgroundIntensity')
+    .min(0)
+    .max(10)
+    .step(0.001);
+
+global.envMapIntensity = 1;
+gui
+    .add(global, 'envMapIntensity')
+    .min(0)
+    .max(10)
+    .step(0.001)
+    .onChange(updateAllMaterials);
+
+// rgbeLoader.load('/environmentMaps/blender-2k.hdr', (envMap) => {
+//     envMap.mapping = THREE.EquirectangularReflectionMapping;
+
+//     scene.background = envMap;
+//     scene.environment = envMap;
+// })
+
+exrLoader.load('/environmentMaps/nvidiaCanvas-4k.exr', (envMap) => {
+    envMap.mapping = THREE.EquirectangularReflectionMapping;
+
+    scene.background = envMap;
+    scene.environment = envMap;
+});
+
+// const cubeTextureLoader = new THREE.CubeTextureLoader();
+// const environmentMap = cubeTextureLoader.load([
+//     '/environmentMaps/0/px.png',
+//     '/environmentMaps/0/nx.png',
+//     '/environmentMaps/0/py.png',
+//     '/environmentMaps/0/ny.png',
+//     '/environmentMaps/0/pz.png',
+//     '/environmentMaps/0/nz.png'
+// ]);
+
+// scene.background = environmentMap;
+// scene.environment = environmentMap;
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.set(4, 5, 4);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(2, devicePixelRatio));
+
+const controls = new OrbitControls(camera, canvas);
+controls.target.y = 3.5;
+controls.enableDamping = true;
+
+const torusKnot = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(1, 0.4, 100, 16),
+    new THREE.MeshStandardMaterial({ roughness: 0.3, metalness: 1, color: 0xaaaaaa })
+);
+torusKnot.position.x = -4;
+torusKnot.position.y = 4;
+scene.add(torusKnot);
+
+gltfLoader.load('/models/FlightHelmet/glTF/FlightHelmet.gltf', 
+    (gltf) => {
+        gltf.scene.scale.set(10, 10, 10);
+        scene.add(gltf.scene);
+
+        updateAllMaterials();
+    }
+);
+
+const clock = new THREE.Clock();
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+
+    controls.update();
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(tick);
+};
+
+tick();
+
+/** CSS
+*
+{
+    margin: 0;
+    padding: 0;
+}
+
+html,
+body
+{
+    overflow: hidden;
+}
+
+.webgl
+{
+    position: fixed;
+    top: 0;
+    left: 0;
+    outline: none;
+}
+*/
