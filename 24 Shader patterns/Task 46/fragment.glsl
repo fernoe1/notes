85af015887619c4bb55a6d241ef2f1d110f61670
fernoe1 <krutoytemirlan2007@mail.ru>
2026-03-16 00:43:42 +0600

    feat(shader patterns): task fourty six

---
+++ 24 Shader patterns/Task 46/fragment.glsl
@@ -0,0 +1,31 @@
+#define PI 3.1415926535897932384626433832795
+
+precision mediump float;
+
+varying vec2 vUv;
+
+float random (vec2 st) {
+    return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
+vec2 rotate(vec2 uv, float rotation, vec2 mid) {
+    return vec2(
+      cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
+      cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
+    );
+}
+
+void main() {
+
+
+    float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    angle /= PI * 2.0;
+    angle += 0.5;
+    float sinusoid = sin(angle * 100.0);
+
+    float radius = 0.25 + sinusoid * 0.02;
+
+    float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - radius));
+
+    gl_FragColor = vec4(vec3(strength), 1.0);
+}
