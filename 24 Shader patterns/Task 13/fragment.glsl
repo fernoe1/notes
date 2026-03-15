cd043907e162fd5a49879ce15a99ec0bb1b7f8f2
fernoe1 <krutoytemirlan2007@mail.ru>
2026-03-15 16:04:10 +0600

    feat(shader patterns): task thirteen

---
+++ 24 Shader patterns/Task 13/fragment.glsl
@@ -0,0 +1,12 @@
+precision mediump float;
+
+varying vec2 vUv;
+
+void main() {
+    float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
+    strength -= step(0.2, mod(vUv.y * 10.0, 1.0));
+
+    // or strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
+
+    gl_FragColor = vec4(vec3(strength), 1.0);
+}
