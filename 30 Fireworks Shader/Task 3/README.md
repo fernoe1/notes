71cb0e4a923b5c56e5defac8574f90adf3526a0f
fernoe1 <krutoytemirlan2007@mail.ru>
2026-03-29 14:25:45 +0600

    feat(fireworks shader): task three

---
+++ 30 Fireworks Shader/Task 3/README.md
@@ -0,0 +1,19 @@
+Continuation from the previous task.
+
+Create uProgress uniform attribute, set it to 0.
+
+Animate uProgress uniform attribute from 0 to 1 in 3 secs with linear easing using gsap. When animation is finished fire function that disposes of the fireworks.
+
+Vertex shader:
+
+Create exploding progress that makes particles explode from the center to their positions.
+
+Create falling progress that makes particles fall.
+
+Create sizeProgress that makes particles scale up then scale down.
+
+Create twinkling progress that makes particels twinkle.
+
+Handle gl_PointSize < 1.0 for windows users.
+
+Create createRandomFirework function that is a wrapper around createFirework, fire it whenever click function is fired.
---
+++ 30 Fireworks Shader/Task 3/solution.js
@@ -0,0 +1,165 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { SkyMesh } from 'three/examples/jsm/objects/SkyMesh.js';
+import GUI from 'lil-gui';
+import fireworkVertexShader from './shaders/firework/vertex.glsl';
+import fireworkFragmentShader from './shaders/firework/fragment.glsl';
+import gsap from 'gsap';
+
+console.log(fireworkVertexShader);
+console.log(fireworkFragmentShader);
+
+const gui = new GUI({
+    title: 'debug',
+    width: 370,
+    closeFolders: true
+});
+
+const textureLoader = new THREE.TextureLoader();
+
+addEventListener('keypress', (e) => {
+    if (e.key === 'h') gui.show(gui._hidden);
+});
+
+const canvas = document.querySelector('canvas.webgl');
+
+const sizes = {
+    width: innerWidth,
+    height: innerHeight,
+    pixelRatio: Math.min(devicePixelRatio, 2)
+};
+sizes.resolution = new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio);
+
+addEventListener('resize', () => {
+    sizes.width = innerWidth;
+    sizes.height = innerHeight;
+    sizes.pixelRatio = Math.min(devicePixelRatio, 2);
+    sizes.resolution.set(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio);
+
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(sizes.pixelRatio);
+});
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(25, sizes.width / sizes.height, 0.1, 100);
+camera.position.set(1.5, 0, 6);
+scene.add(camera);
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(sizes.pixelRatio);
+
+const textures = [
+    textureLoader.load('./particles/1.png'),
+    textureLoader.load('./particles/2.png'),
+    textureLoader.load('./particles/3.png'),
+    textureLoader.load('./particles/4.png'),
+    textureLoader.load('./particles/5.png'),
+    textureLoader.load('./particles/6.png'),
+    textureLoader.load('./particles/7.png'),
+    textureLoader.load('./particles/8.png')
+];
+
+// Fireworks
+const createFirework = (count, position, size, texture, radius, color) => {
+    const positionsArray = new Float32Array(count * 3);
+    const sizesArray = new Float32Array(count);
+    const timeMultipliersArray = new Float32Array(count);
+
+    for (let i = 0; i < count; i++) {
+        const i3 = i * 3;
+
+        const spherical = new THREE.Spherical(
+            radius * (0.75 + Math.random() * 0.25),
+            Math.random() * Math.PI,
+            Math.random() * Math.PI * 2
+        );
+        const position = new THREE.Vector3();
+        position.setFromSpherical(spherical);
+
+        positionsArray[i3    ] = position.x;
+        positionsArray[i3 + 1] = position.y;
+        positionsArray[i3 + 2] = position.z;
+
+        sizesArray[i] = Math.random();
+
+        timeMultipliersArray[i] = 1 + Math.random();
+    }
+
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(positionsArray, 3));
+    geometry.setAttribute('aSize', new THREE.Float32BufferAttribute(sizesArray, 1));
+    geometry.setAttribute('aTimeMultiplier', new THREE.Float32BufferAttribute(timeMultipliersArray, 1));
+
+    texture.flipY = false;
+    const material = new THREE.ShaderMaterial({
+        vertexShader: fireworkVertexShader,
+        fragmentShader: fireworkFragmentShader,
+        uniforms: {
+            uSize: new THREE.Uniform(size),
+            uResolution: new THREE.Uniform(sizes.resolution),
+            uTexture: new THREE.Uniform(texture),
+            uColor: new THREE.Uniform(color),
+            uProgress: new THREE.Uniform(0)
+        },
+        transparent: true,
+        depthWrite: false,
+        blending: THREE.AdditiveBlending
+    });
+
+    const fireworks = new THREE.Points(
+        geometry,
+        material
+    );
+    fireworks.position.copy(position);
+    scene.add(fireworks);
+
+    const destroy = () => {
+        scene.remove(fireworks);
+        material.dispose();
+        geometry.dispose();
+    }
+
+    gsap.to(
+        material.uniforms.uProgress,
+        { value: 1, duration: 3, ease: 'linear', onComplete: destroy }
+    );
+};
+
+const createRandomFirework = () => {
+    const count = Math.round(400 + Math.random() * 1000);
+    const position = new THREE.Vector3(
+        (Math.random() - 0.5) * 2,
+        Math.random(),
+        (Math.random() - 0.5) * 2
+    );
+    const size = 0.1 + Math.random() * 0.1;
+    const texture = textures[Math.floor(Math.random() * textures.length)];
+    const radius = 0.5 + Math.random();
+    const color = new THREE.Color();
+    color.setHSL(Math.random(), 1, 0.7);
+
+    createFirework(count, position, size, texture, radius, color);
+}
+
+addEventListener('click', createRandomFirework);
+
+const tick = () => {
+    controls.update();
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(tick);
+};
+
+tick();
