906d06ba025709f2d9ed2c5519a0f775d59fa790
fernoe1 <krutoytemirlan2007@mail.ru>
2026-05-03 01:18:12 +0500

    feat(fr3fa): task eleven

---
+++ 50 First React Three Fiber Application/Task 11/Experience.jsx
@@ -0,0 +1,53 @@
+import { useThree, extend, useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import CustomObject from './CustomObject.jsx';
+
+extend({ OrbitControls })
+
+const Experience = () => {
+    const { camera, gl } = useThree();
+    const cube = useRef();
+    const group = useRef();
+
+    useFrame((st, del) => {
+        cube.current.rotation.y += del;
+        // group.current.rotation.y += del;
+
+        const angle = st.clock.getElapsedTime();
+
+        st.camera.position.x = Math.sin(angle) * 8;
+        st.camera.position.z = Math.cos(angle) * 8;
+        st.camera.lookAt(0, 0, 0)
+    });
+
+    return <>
+
+        {/* <orbitControls args={ [ camera, gl.domElement ] } /> */}
+
+        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+        <ambientLight intensity={ 0.5 } />
+
+        <group ref={ group } >
+            <mesh ref={ cube } position-x={ 2 } scale={ 1.5 } >
+                <boxGeometry />
+                <meshStandardMaterial color={ 'mediumpurple' } />
+            </mesh>
+
+            <mesh position-x={ -2 } >
+                <sphereGeometry />
+                <meshStandardMaterial color={ 'orange' } />
+            </mesh>
+        </group>
+
+        <mesh rotation-x={ -Math.PI * 0.5 } position-y={ -1 } scale={ 10 } >
+            <planeGeometry />
+            <meshStandardMaterial color={ 'greenyellow' } />
+        </mesh>
+
+        <CustomObject />
+
+    </>
+};
+
+export default Experience;
---
+++ 50 First React Three Fiber Application/Task 11/README.md
@@ -0,0 +1,10 @@
+Continuation from the previous task.
+
+
+
+Inside Experience.jsx component
+
+Disable orbitControls by commenting it.
+
+Animate the camera circling around the scene using state.camera, sin, cos, and state.clock.
+
