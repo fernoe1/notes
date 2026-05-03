81751e999d64e11e3f00b80839bd6e67174316bb
fernoe1 <krutoytemirlan2007@mail.ru>
2026-05-04 00:20:49 +0500

    feat(r3f && drei): task three

---
+++ 51 R3F and Drei/Task 3/Experience.jsx
@@ -0,0 +1,46 @@
+import { 
+    PivotControls,
+    OrbitControls,
+    TransformControls
+} from '@react-three/drei';
+import { useRef } from 'react';
+
+const Experience = () => {
+    const cube = useRef();
+
+    return <>
+
+        <OrbitControls makeDefault />
+
+        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
+        <ambientLight intensity={ 1.5 } />
+
+        <PivotControls 
+            anchor={ [ 0, 0, 0 ] }
+            depthTest={ false }
+            lineWidth={ 4 }
+            axisColors={ [ '#9381ff', '#ff4d6d', '#7ae582' ] }
+            scale={ 100 }
+            fixed={ true }
+        >
+            <mesh position-x={ - 2 }>
+                <sphereGeometry />
+                <meshStandardMaterial color="orange" />
+            </mesh>
+        </PivotControls>
+
+        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+        <TransformControls object={ cube }/>
+
+        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+            <planeGeometry />
+            <meshStandardMaterial color="greenyellow" />
+        </mesh>
+
+    </>
+};
+
+export default Experience;
---
+++ 51 R3F and Drei/Task 3/README.md
@@ -0,0 +1,7 @@
+Continuation from the previous task.
+
+Import PivotControls, and wrap sphere mesh using it.
+
+Set PivotControls attributes; anchor to 0, 0, 0 (relative to mesh), depthTest false, lineWidth 4, axisColors to colors you want, scale 100, fixed true.
+
+Compared to TransformControls, PivotControls is not a group, so you have to create one PivotControls for each mesh you want it to have.
