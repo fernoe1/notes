b41f95f6d33181810ffb668f6f4d10eeb4848e6c
fernoe1 <krutoytemirlan2007@mail.ru>
2026-05-03 23:57:28 +0500

    feat(r3f && drei): task two

---
+++ 51 R3F and Drei/Task 2/Experience.jsx
@@ -0,0 +1,36 @@
+import { 
+    OrbitControls,
+    TransformControls
+} from '@react-three/drei';
+import { useRef } from 'react';
+
+const Experience = () => {
+    const cube = useRef();
+
+    return <>
+
+        <OrbitControls makeDefault />
+
+        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
+        <ambientLight intensity={ 1.5 } />
+
+        <mesh position-x={ - 2 }>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+        </mesh>
+
+        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+        <TransformControls object={ cube }/>
+
+        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+            <planeGeometry />
+            <meshStandardMaterial color="greenyellow" />
+        </mesh>
+
+    </>
+};
+
+export default Experience;
---
+++ 51 R3F and Drei/Task 2/README.md
@@ -0,0 +1,26 @@
+Continuation from the previous task.
+
+
+
+Import TransformControls and attach it to the purple cube.
+
+
+
+Theres two options.
+
+
+
+Wrap cube in TransformControls and pass the position of cube to it.
+
+
+
+Use useRef hook to ref cube and pass the ref to object attribute of TransformControls.
+
+
+
+Use the second option.
+
+
+
+Set OrbitControls as default so the camera doesn't move when you tinker with controls.
+
