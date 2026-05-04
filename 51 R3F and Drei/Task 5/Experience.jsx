fd8a311a7e873d4058d17f376fa4b1333923558f
fernoe1 <krutoytemirlan2007@mail.ru>
2026-05-04 10:18:08 +0500

    feat(r3f && drei): task five

---
+++ 51 R3F and Drei/Task 5/Experience.jsx
@@ -0,0 +1,69 @@
+import { 
+    Text,
+    Html,
+    PivotControls,
+    OrbitControls,
+    TransformControls
+} from '@react-three/drei';
+import { useRef } from 'react';
+
+const Experience = () => {
+    const cube = useRef();
+    const sphere = useRef();
+
+    return <>
+
+        <OrbitControls makeDefault />
+
+        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
+        <ambientLight intensity={ 1.5 } />
+
+        <PivotControls 
+            anchor={ [ 0, 0, 0 ] }
+            depthTest={ false }
+            lineWidth={ 4 }
+            axisColors={ [ '#9381ff', '#ff4d6d', '#7ae582' ] }
+            scale={ 100 }
+            fixed={ true }
+        >
+            <mesh ref={ sphere } position-x={ - 2 }>
+                <sphereGeometry />
+                <meshStandardMaterial color="orange" />
+                <Html
+                    position={ [ 1, 1, 0 ] }
+                    wrapperClass={ 'label' }
+                    center
+                    distanceFactor={ 8 }
+                    occlude={ [ sphere, cube ] }
+                >
+                    Sphere
+                </Html>
+            </mesh>
+        </PivotControls>
+
+        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+        <TransformControls object={ cube }/>
+
+        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+            <planeGeometry />
+            <meshStandardMaterial color="greenyellow" />
+        </mesh>
+
+        <Text
+            font={ './bangers-v20-latin-regular.woff' }
+            fontSize={ 1 }
+            color={ 'salmon' }
+            position-y={ 2 }
+            maxWidth={ 2 }
+            textAlign={ 'center' }
+        >
+            I LOVE R3F
+        </Text>
+
+    </>
+};
+
+export default Experience;
---
+++ 51 R3F and Drei/Task 5/README.md
@@ -0,0 +1,17 @@
+Continuation from the previous task.
+
+Import Text from @react-three/drei.
+
+Text component uses SDF (signed distance fields) under the hood.
+
+Set the text font to the one inside public folder.
+
+Set the size to 1.
+
+Set the color to salmon.
+
+Set the position to 0, 2, 0.
+
+Set the max width to 2.
+
+Align the text to the center.
