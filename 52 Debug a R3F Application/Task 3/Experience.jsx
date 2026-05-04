8c7c43c9c5410c59f972da9f0d32266da7d10d5c
fernoe1 <krutoytemirlan2007@mail.ru>
2026-05-04 13:03:51 +0500

    feat(debug r3f): task three

---
+++ 52 Debug a R3F Application/Task 3/Experience.jsx
@@ -0,0 +1,39 @@
+import { OrbitControls } from '@react-three/drei';
+import { useControls } from 'leva';
+
+const Experience = () => {
+    const { position } = useControls({ 
+        position: {
+            value: -2,
+            min: -4,
+            max: 4,
+            step: 0.01
+        }
+    });
+
+    return <>
+
+        <OrbitControls makeDefault />
+
+        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
+        <ambientLight intensity={ 1.5 } />
+
+        <mesh position-x={ position }>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+        </mesh>
+
+        <mesh position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+
+        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+            <planeGeometry />
+            <meshStandardMaterial color="greenyellow" />
+        </mesh>
+
+    </>
+};
+
+export default Experience;
---
+++ 52 Debug a R3F Application/Task 3/README.md
@@ -0,0 +1,14 @@
+Continuation from the previous task.
+
+
+
+Install leva@0.9 using npm.
+
+
+
+Import useControls from leva inside Experience.jsx component.
+
+
+
+Using useControls destructure controls into position that can be changed using slider, with value 2, min -4, max 4, and step 0.01.
+
