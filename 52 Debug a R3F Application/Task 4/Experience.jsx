c6d994c02a93dc47aacd7c8e4ca308bf6aaf0538
fernoe1 <krutoytemirlan2007@mail.ru>
2026-05-05 09:32:09 +0500

    feat(debug r3f): task four

---
+++ 52 Debug a R3F Application/Task 4/Experience.jsx
@@ -0,0 +1,56 @@
+import { OrbitControls } from '@react-three/drei';
+import { button, useControls } from 'leva';
+
+const Experience = () => {
+    const { position, color, visible } = useControls('sphere', { 
+        position: {
+            value: { x: -2, y: 0 },
+            step: 0.01,
+            joystick: 'invertY'
+        },
+        color: 'orange',
+        visible: true,
+        interval: {
+            min: 0,
+            max: 10,
+            value: [ 4, 5 ]
+        },
+        click: button(() => { console.log('ok') } ),
+        choice: { options: [ 'a', 'b', 'c' ] }
+    });
+
+    const { scale } = useControls('cube', {
+        scale: {
+            value: 1.5,
+            step: 0.01,
+            min: 0,
+            max: 5
+        }
+    });
+
+    return <>
+    
+        <OrbitControls makeDefault />
+
+        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
+        <ambientLight intensity={ 1.5 } />
+
+        <mesh position={ [ position.x, position.y, 0 ] } visible={ visible } >
+            <sphereGeometry />
+            <meshStandardMaterial color={ color } />
+        </mesh>
+
+        <mesh position-x={ 2 } scale={ scale } >
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+
+        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+            <planeGeometry />
+            <meshStandardMaterial color="greenyellow" />
+        </mesh>
+
+    </>
+};
+
+export default Experience;
---
+++ 52 Debug a R3F Application/Task 4/README.md
@@ -0,0 +1,25 @@
+Continuation from the previous task.
+
+In Leva:
+
+Change position into a joystick where you can change both x and y values with a step of 0.01. Set joystick property to 'invertY'.
+
+Add color property.
+
+Add visible checkbox.
+
+Create interval with min 0 max 0 values 4 and 5.
+
+Create button using button from Leva.
+
+Create choice options using options with array of possible values.
+
+Use folders:
+
+Put all properties of sphere inside sphere folder.
+
+Create new cube folder with scale propety.
+
+Import Leva in index.jsx.
+
+Use Leva component to make it collapse at the start.
