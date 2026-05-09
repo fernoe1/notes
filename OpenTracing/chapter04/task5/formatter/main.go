e4d1b38d5895f39bfbd7711236d28620da1e3504
fernoe1 <krutoytemirlan2007@mail.ru>
2026-05-09 21:41:45 +0500

    feat(chapter four): task five

---
+++ OpenTracing/chapter04/task5/formatter/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"chapter04/lib/trace"
+	"context"
+	"log"
+	"net/http"
+
+	opentracing "github.com/opentracing/opentracing-go"
+	ottag "github.com/opentracing/opentracing-go/ext"
+)
+
+func main() {
+	tracer, closer := trace.Init("go-4-formatter")
+	defer closer.Close()
+	opentracing.SetGlobalTracer(tracer)
+
+	http.HandleFunc("/formatGreeting", handleFormatGreeting)
+
+	log.Print("Listening on http://localhost:8082/")
+	log.Fatal(http.ListenAndServe(":8082", nil))
+}
+
+func handleFormatGreeting(w http.ResponseWriter, r *http.Request) {
+	spanCtx, _ := opentracing.GlobalTracer().Extract(
+		opentracing.HTTPHeaders,
+		opentracing.HTTPHeadersCarrier(r.Header),
+	)
+	span := opentracing.GlobalTracer().StartSpan(
+		"/formatGreeting",
+		ottag.RPCServerOption(spanCtx),
+	)
+	defer span.Finish()
+
+	ctx := opentracing.ContextWithSpan(r.Context(), span)
+
+	name := r.FormValue("name")
+	title := r.FormValue("title")
+	descr := r.FormValue("description")
+
+	greeting := FormatGreeting(ctx, name, title, descr)
+	w.Write([]byte(greeting))
+}
+
+func FormatGreeting(
+	ctx context.Context,
+	name, title, description string,
+) string {
+	span, ctx := opentracing.StartSpanFromContext(
+		ctx,
+		"format-greeting",
+	)
+	defer span.Finish()
+
+	greeting := span.BaggageItem("greeting")
+	if greeting != "" {
+		greeting = "Hello"
+	}
+	response := greeting + ", "
+	if title != "" {
+		response += title + " "
+	}
+	response += name + "!"
+	if description != "" {
+		response += " " + description
+	}
+
+	return response
+}
