d2909f7913a986239b9e8bb6c849db1d2bda8aa6
fernoe1 <krutoytemirlan2007@mail.ru>
2026-05-09 20:35:13 +0500

    feat(chapter four): task three a

---
+++ OpenTracing/chapter04/task3a/hello.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"chapter04/lib/trace"
+	"chapter04/task1/people"
+	"log"
+	"net/http"
+	"strings"
+
+	opentracing "github.com/opentracing/opentracing-go"
+	otlog "github.com/opentracing/opentracing-go/log"
+)
+
+var repo *people.Repository
+var tracer opentracing.Tracer
+
+func main() {
+	repo = people.NewRepository()
+	defer repo.Close()
+
+	tr, closer := trace.Init("go-3-hello")
+	defer closer.Close()
+
+	tracer = tr
+
+	http.HandleFunc("/sayHello/", handleSayHello)
+
+	log.Print("Listening on http://localhost:8080/")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleSayHello(w http.ResponseWriter, r *http.Request) {
+	span := tracer.StartSpan("say-hello")
+	defer span.Finish()
+
+	name := strings.TrimPrefix(r.URL.Path, "/sayHello/")
+	greeting, err := SayHello(name, span)
+	if err != nil {
+		span.SetTag("error", true)
+		span.LogFields(otlog.Error(err))
+
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+
+		return
+	}
+
+	w.Write([]byte(greeting))
+}
+
+func SayHello(name string, span opentracing.Span) (string, error) {
+	person, err := repo.GetPerson(name)
+	if err != nil {
+
+		return "", err
+	}
+
+	span.LogKV(
+		"name", person.Name,
+		"title", person.Title,
+		"description", person.Description,
+	)
+
+	return FormatGreeting(
+		person.Name,
+		person.Title,
+		person.Description,
+	), nil
+}
+
+func FormatGreeting(name, title, description string) string {
+	response := "Hello, "
+	if title != "" {
+		response += title + " "
+	}
+	response += name + "!"
+	if description != "" {
+		response += " " + description
+	}
+
+	return response
+}
